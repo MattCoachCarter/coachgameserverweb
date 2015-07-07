2d41bcb494a780463e27b6bfaac6edc45902ca98
Matt Carter <matt.coach.carter@gmail.com>
2015-07-07 17:53:11 -0500

    Debugging stubbed out API

--- phpapi/terraria_server_log.php
+++ phpapi/terraria_server_log.php
@@ -199,6 +199,6 @@ Coach has joined.
 <Coach> test2
 Coach has left.";
 
-echo '{ "lines": 6, "content": "'.str_replace("\r", '', str_replace("\n", '~*~', $content)).'"; }';
+echo '{ "lines": 6, "content": "'.str_replace("\r", '', str_replace("\n", '~*~', $content)).'" }';
 
 ?>
