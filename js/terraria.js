56b2f51bfaea464ea5a138e55f8826b4056e7856
Matt Carter <matt.coach.carter@gmail.com>
2015-07-07 17:40:52 -0500

    Updated stubbed out API

--- js/terraria.js
+++ js/terraria.js
@@ -2,13 +2,14 @@
 
 $(function()
 {
-  window.serverLogLines = 0;
-  window.chatNameColors = {};
-  window.chatColors = [ '#FF0000', '#000099', '#CC6600', '#009933', '#9933FF' ];
-  window.chatColorsIndex = 0;
+	console.log
+  	window.serverLogLines = 0;
+  	window.chatNameColors = {};
+  	window.chatColors = [ '#FF0000', '#000099', '#CC6600', '#009933', '#9933FF' ];
+  	window.chatColorsIndex = 0;
 
-  setUpTabbedContent();
-  getServerStatus();
+  	setUpTabbedContent();
+  	getServerStatus();
 	getServerLog();
 });
 
@@ -22,82 +23,82 @@ function setUpTabbedContent()
 
 function getServerStatus()
 {
-  $.get( 'phpapi/terraria_server_status.php', function( data )
-  {
-    var serverStatus = JSON.parse(data);
-    $('#server_status_' + String(serverStatus.status)).show();
-  })
-  .fail(function()
-  {
-    $('#server_status_failed').show();
-  })
-  .always(function()
-  {
-    $('#server_status_calculating').hide();
-  });
+  	$.get( 'phpapi/terraria_server_status.php', function( data )
+  	{
+    	var serverStatus = JSON.parse(data);
+    	$('#server_status_' + String(serverStatus.status)).show();
+  	})
+  	.fail(function()
+  	{
+    	$('#server_status_failed').show();
+  	})
+  	.always(function()
+  	{
+    	$('#server_status_calculating').hide();
+  	});
 }
 
 function makeLogLine(_line)
 {
-  return '<div class="logline">'+ _line +'</div>';
+  	return '<div class="logline">'+ _line +'</div>';
 }
 
 function makeChatLine(_line)
 {
-  var chatName = '';
+  	var chatName = '';
 
-  if(_line.indexOf('<') === 0)
-  {
-    chatName = _line.split('>')[0].replace('<', '');
-  }
-  else if(_line.indexOf('has left') !== -1)
-  {
-    chatName = _line.replace(/ has left.*/, '');
-  }
-  else if(_line.indexOf('has joined') !== -1)
-  {
-    chatName = _line.replace(/ has joined.*/, '');
-  }
+  	if(_line.indexOf('<') === 0)
+  	{
+   	chatName = _line.split('>')[0].replace('<', '');
+  	}
+  	else if(_line.indexOf('has left') !== -1)
+  	{
+    	chatName = _line.replace(/ has left.*/, '');
+  	}
+  	else if(_line.indexOf('has joined') !== -1)
+  	{
+    	chatName = _line.replace(/ has joined.*/, '');
+  	}
+	
+  	var chatColor = window.chatNameColors[chatName];
+  	if(chatColor === undefined)
+  	{
+    	window.chatNameColors[chatName] = window.chatColors[window.chatColorsIndex];
+    	chatColor = window.chatColors[window.chatColorsIndex];
+    	window.chatColorsIndex++;
+  	}
 
-  var chatColor = window.chatNameColors[chatName];
-  if(chatColor === undefined)
-  {
-    window.chatNameColors[chatName] = window.chatColors[window.chatColorsIndex];
-    chatColor = window.chatColors[window.chatColorsIndex];
-    window.chatColorsIndex++;
-  }
 
-
-  return '<div class="chatline"><span class="chatname" style="color: '+chatColor+';">&lt;'+chatName+'&gt;'+_line.sub('<'+chatName+'>', '')+'</div>';
+  	return '<div class="chatline"><span class="chatname" style="color: '+chatColor+';">&lt;'+chatName+'&gt;'+_line.sub('<'+chatName+'>', '')+'</div>';
 }
 
 function processLogContents(_contents)
 {
-  var logContent = '';
-  var chatContent = '';
+  	var logContent = '';
+  	var chatContent = '';
 
-  var contentsSplit = String(_contents).split('\n');
+  	var contentsSplit = String(_contents).split('\n');
 
-  for(var i = 0; i < contentsSplit.length; i++)
-  {
-    if(contentsSplit[i].indexOf('<') === 0 || contentsSplit[i].indexOf('has joined') !== -1 || contentsSplit[i].indexOf('has left') !== -1)
-    {
-      chatContent += makeChatLine(contentsSplit[i]);
-    }
-
-    logContent += makeLogLine(contentsSplit[i]);
-  }
-
-  $('#server_chat').append(chatContent);
-  $('#server_log').append(logContent);
+  	for(var i = 0; i < contentsSplit.length; i++)
+  	{
+    	if(contentsSplit[i].indexOf('<') === 0 || contentsSplit[i].indexOf('has joined') !== -1 || contentsSplit[i].indexOf('has left') !== -1)
+    	{
+	      chatContent += makeChatLine(contentsSplit[i]);
+    	}
+	
+    	logContent += makeLogLine(contentsSplit[i]);
+  	}
+	
+  	$('#server_chat').append(chatContent);
+  	$('#server_log').append(logContent);
 }
 
 function getServerLog()
 {
-  $.get( 'phpapi/terraria_server_log.php?lines_seen=' + window.serverLogLines, function( data )
-  {
-    var serverLog = JSON.parse(data);
-    window.serverLogLines = serverLog.lines;
-    processLogContents(serverLog.content);
-  });
+  	$.get( 'phpapi/terraria_server_log.php?lines_seen=' + window.serverLogLines, function( data )
+  	{
+    	var serverLog = JSON.parse(data);
+    	window.serverLogLines = serverLog.lines;
+    	processLogContents(serverLog.content);
+  	});
 }
--- phpapi/terraria_server_log.php
+++ phpapi/terraria_server_log.php
@@ -2,5 +2,203 @@
   $lines_seen = $_GET['lines_seen'];
 
   //Right now just return some stuff to stub out the file
-  echo "asdf\njkl;\nasdf\njkl;\nasdf\njkl;";
+$content = "Terraria Server v1.3.0.3
+
+Resetting game objects 10%
+Resetting game objects 11%
+Resetting game objects 12%
+Resetting game objects 13%
+Resetting game objects 14%
+Resetting game objects 15%
+Resetting game objects 16%
+Resetting game objects 17%
+Resetting game objects 18%
+Resetting game objects 19%
+Resetting game objects 20%
+Resetting game objects 21%
+Resetting game objects 22%
+Resetting game objects 23%
+Resetting game objects 24%
+Resetting game objects 25%
+Resetting game objects 26%
+Resetting game objects 27%
+Resetting game objects 28%
+Resetting game objects 29%
+Resetting game objects 30%
+Resetting game objects 31%
+Resetting game objects 32%
+Resetting game objects 33%
+Resetting game objects 34%
+Resetting game objects 35%
+Resetting game objects 36%
+Resetting game objects 37%
+Resetting game objects 38%
+Resetting game objects 39%
+Resetting game objects 43%
+Resetting game objects 44%
+Resetting game objects 45%
+Resetting game objects 46%
+Resetting game objects 47%
+Resetting game objects 48%
+Resetting game objects 49%
+Resetting game objects 50%
+Resetting game objects 51%
+Resetting game objects 52%
+Resetting game objects 53%
+Resetting game objects 54%
+Resetting game objects 55%
+Resetting game objects 56%
+Resetting game objects 57%
+Resetting game objects 59%
+Resetting game objects 60%
+Resetting game objects 61%
+Resetting game objects 62%
+Resetting game objects 63%
+Resetting game objects 64%
+Resetting game objects 65%
+Resetting game objects 69%
+Resetting game objects 70%
+Resetting game objects 71%
+Resetting game objects 72%
+Resetting game objects 73%
+Resetting game objects 74%
+Resetting game objects 75%
+Resetting game objects 76%
+Resetting game objects 79%
+Resetting game objects 80%
+Resetting game objects 81%
+Resetting game objects 82%
+Resetting game objects 85%
+Resetting game objects 86%
+Resetting game objects 87%
+Resetting game objects 88%
+Resetting game objects 91%
+Resetting game objects 92%
+Resetting game objects 93%
+Resetting game objects 94%
+Resetting game objects 95%
+Resetting game objects 98%
+Resetting game objects 99%
+Resetting game objects 100%
+Loading world data: 1%
+Loading world data: 2%
+Loading world data: 3%
+Loading world data: 4%
+Loading world data: 5%
+Loading world data: 6%
+Loading world data: 7%
+Loading world data: 8%
+Loading world data: 9%
+Loading world data: 10%
+Loading world data: 11%
+Loading world data: 12%
+Loading world data: 13%
+Loading world data: 14%
+Loading world data: 15%
+Loading world data: 16%
+Loading world data: 17%
+Loading world data: 18%
+Loading world data: 19%
+Loading world data: 20%
+Loading world data: 21%
+Loading world data: 22%
+Loading world data: 23%
+Loading world data: 24%
+Loading world data: 25%
+Loading world data: 26%
+Loading world data: 27%
+Loading world data: 28%
+Loading world data: 29%
+Loading world data: 30%
+Loading world data: 31%
+Loading world data: 32%
+Loading world data: 33%
+Loading world data: 34%
+Loading world data: 35%
+Loading world data: 36%
+Loading world data: 37%
+Loading world data: 38%
+Loading world data: 39%
+Loading world data: 40%
+Loading world data: 41%
+Loading world data: 42%
+Loading world data: 43%
+Loading world data: 44%
+Loading world data: 45%
+Loading world data: 100%
+Settling liquids 1%
+Settling liquids 2%
+Settling liquids 3%
+Settling liquids 4%
+Settling liquids 5%
+Settling liquids 6%
+Settling liquids 7%
+Settling liquids 8%
+Settling liquids 9%
+Settling liquids 10%
+Settling liquids 11%
+Settling liquids 12%
+Settling liquids 13%
+Settling liquids 14%
+Settling liquids 15%
+Settling liquids 16%
+Settling liquids 17%
+Settling liquids 18%
+Settling liquids 19%
+Settling liquids 20%
+Settling liquids 21%
+Settling liquids 22%
+Settling liquids 23%
+Settling liquids 24%
+Settling liquids 25%
+Settling liquids 26%
+Settling liquids 27%
+Settling liquids 28%
+Settling liquids 29%
+Settling liquids 30%
+Settling liquids 31%
+Settling liquids 32%
+Settling liquids 33%
+Settling liquids 34%
+Settling liquids 35%
+Settling liquids 36%
+Settling liquids 37%
+Settling liquids 38%
+Settling liquids 39%
+Settling liquids 40%
+Settling liquids 41%
+Settling liquids 42%
+Settling liquids 43%
+Settling liquids 44%
+Settling liquids 45%
+Settling liquids 46%
+Settling liquids 47%
+Settling liquids 48%
+Settling liquids 49%
+Settling liquids 50%
+Settling liquids 92%
+Settling liquids 96%
+Settling liquids 97%
+Settling liquids 98%
+Settling liquids 99%
+Starting server...
+Server started
+Terraria Server v1.3.0.3
+
+Listening on port 7777
+Type 'help' for a list of commands.
+
+: 24.107.145.104:52377 is connecting...
+24.107.145.104:52377 is connecting...
+Coach has joined.
+Coach has left.
+24.107.145.104:52599 is connecting...
+24.107.145.104:52599 is connecting...
+Coach has joined.
+<Coach> test
+<Coach> test2
+Coach has left."
+
+echo '{ "lines": 6, "content": '.$content.'; }';
+
 ?>
