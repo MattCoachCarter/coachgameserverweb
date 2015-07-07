9de07f4509f6ce4052c79695002f07f5d1c5c2a5
Matthew Carter <mcarter@exegy.com>
2015-07-07 12:21:42 -0500

    Adding content to terraria page and stubbing out some of the API

---
+++ phpapi/terraria_server_log.php
@@ -0,0 +1,6 @@
+<?php
+  $lines_seen = $_GET['lines_seen'];
+
+  //Right now just return some stuff to stub out the file
+  echo "asdf\njkl;\nasdf\njkl;\nasdf\njkl;";
+?>
---
+++ phpapi/terraria_server_status.php
@@ -0,0 +1,4 @@
+<?php
+  //Right now just return up until I have a better way to do this
+  echo '{ "status": "up" }';
+?>
