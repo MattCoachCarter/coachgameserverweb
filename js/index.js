a7f68d767cc676b9070f334eaf31930e77c7f7fa
Matt Carter <matt.coach.carter@gmail.com>
2015-07-07 22:39:53 -0500

    Adding API for checking disk usage and displaying it on home page

--- js/index.js
+++ js/index.js
@@ -20,6 +20,14 @@ function getCPULoadPercentage()
 	});
 }
 
+function getDiskUsage()
+{
+	$.get( 'phpapi/c_disk_usage.php', function( data )
+	{
+  		showDiskUsage(data);
+	});
+}
+
 function showCPULoadPercentage(_json)
 {
 	var data = JSON.parse(_json);
@@ -28,26 +36,7 @@ function showCPULoadPercentage(_json)
 	progress_bar.attr('aria-valuenow', String(load_percentage));
 	progress_bar.attr('style', 'width: '+load_percentage+'%; min-width: 2em;');
 	progress_bar.html(String(load_percentage) + '%');
-
-	//Check the percentage and determine what color to make the bar
-	if(load_percentage > 90)
-	{
-		progress_bar.removeClass('progress-bar-warning');
-		progress_bar.addClass('progress-bar-danger');
-	}
-	else if(load_percentage > 50)
-	{
-		progress_bar.removeClass('progress-bar-danger');
-		progress_bar.addClass('progress-bar-warning');
-	}
-	else
-	{
-		progress_bar.removeClass('progress-bar-warning');
-		progress_bar.removeClass('progress-bar-danger');
-	}
-
-	progress_bar.removeClass('active');
-	progress_bar.removeClass('progress-bar-striped');
+	colorizeProgressBar(load_percentage, progress_bar);
 }
 
 function showMemoryInfo(_json)
@@ -65,24 +54,48 @@ function showMemoryInfo(_json)
 
 	progress_bar.attr('style', 'width: '+percentage+'%; min-width: 2em;');
 	progress_bar.html(String(percentage) + '%');
+	colorizeProgressBar(percentage, progress_bar);
+}
 
+function showDiskUsage(_json)
+{
+	var data = JSON.parse(_json);
+	var free = parseInt(data.total_free_bytes);
+	var max = parseInt(data.total_bytes);
+	var used = max - free;
+	var percentage = ((used/max) * 100).toFixed(2);
+	var used_in_gb = (((used/1024)/1024)/1000).toFixed(2);
+	var max_in_gb = (((max/1024)/1024)/1000).toFixed(2);
+
+	var progress_bar = $("#disk_usage_bar");
+
+	progress_bar.attr('aria-valuenow', String(used_in_gb));
+	progress_bar.attr('aria-valuemax', String(max_in_gb));
+
+	progress_bar.attr('style', 'width: '+percentage+'%; min-width: 2em;');
+	progress_bar.html(String(used_in_gb) + '/' + String(max_in_gb) + '%');
+	colorizeProgressBar(percentage, progress_bar);
+}
+
+function colorizeProgressBar(_percentage, _progressBar)
+{
 	//Check the percentage and determine what color to make the bar
-	if(percentage > 90)
+	if(_percentage > 90)
 	{
-		progress_bar.removeClass('progress-bar-warning');
-		progress_bar.addClass('progress-bar-danger');
+		_progressBar.removeClass('progress-bar-warning');
+		_progressBar.addClass('progress-bar-danger');
 	}
-	else if(percentage > 50)
+	else if(_percentage > 50)
 	{
-		progress_bar.removeClass('progress-bar-danger');
-		progress_bar.addClass('progress-bar-warning');
+		_progressBar.removeClass('progress-bar-danger');
+		_progressBar.addClass('progress-bar-warning');
 	}
 	else
 	{
-		progress_bar.removeClass('progress-bar-warning');
-		progress_bar.removeClass('progress-bar-danger');
+		_progressBar.removeClass('progress-bar-warning');
+		_progressBar.removeClass('progress-bar-danger');
 	}
 
-	progress_bar.removeClass('active');
-	progress_bar.removeClass('progress-bar-striped');
+	_progressBar.removeClass('active');
+	_progressBar.removeClass('progress-bar-striped');
 }
