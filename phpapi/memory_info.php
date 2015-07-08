ceba173d08154425491234f2625a65d50493bdf2
Matt Carter <matt.coach.carter@gmail.com>
2015-07-07 21:50:36 -0500

    Removing extra whitespace from memory API call

--- phpapi/memory_info.php
+++ phpapi/memory_info.php
@@ -13,7 +13,7 @@
 		}
 
 		$command_output_split = explode(':', $command_output[$i]);
-		$json .= " \"".strtolower($command_output_split[0])."\": \"".$command_output_split[1]."\"";
+		$json .= " \"".strtolower($command_output_split[0])."\": \"".trim($command_output_split[1])."\"";
 	}
 	$json .= "}";
 
