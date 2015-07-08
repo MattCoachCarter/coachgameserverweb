23aab5811872673802ab75567980608f9c4c2159
Matt Carter <matt.coach.carter@gmail.com>
2015-07-07 22:11:34 -0500

    Adding server health to home page

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function getMemoryInfo()
 {
 	$.get( 'phpapi/memory_info.php', function( data )
 	{
-  		console.log(data);
+  		showMemoryInfo(data);
 	});
 }
 
@@ -16,6 +16,67 @@ function getCPULoadPercentage()
 {
 	$.get( 'phpapi/cpu_load_percentage.php', function( data )
 	{
-  		console.log(data);
+  		showCPULoadPercentage(data);
 	});
+}
+
+function showCPULoadPercentage(_json)
+{
+	var data = JSON.parse(_json);
+	var load_percentage = parseInt(data.load_percentage);
+	var progress_bar = $("#cpu_progress_bar");
+	progress_bar.attr('aria-valuenow', String(load_percentage));
+	progress_bar.attr('style', 'width: '+load_percentage+'%; min-width: 2em;');
+	progress_bar.html(String(load_percentage) + '%');
+
+	//Check the percentage and determine what color to make the bar
+	if(load_percentage > 90)
+	{
+		progress_bar.removeClass('progress-bar-warning');
+		progress_bar.addClass('progress-bar-danger');
+	}
+	else if(load_percentage > 50)
+	{
+		progress_bar.removeClass('progress-bar-danger');
+		progress_bar.addClass('progress-bar-warning');
+	}
+	else
+	{
+		progress_bar.removeClass('progress-bar-warning');
+		progress_bar.removeClass('progress-bar-danger');
+	}
+}
+
+function showMemoryInfo()
+{
+	var data = JSON.parse(_json);
+	var free = parseInt(String(data.available_physical_memory).replace(',', '').replace(' MB', ''));
+	var max = parseInt(String(data.total_physical_memory).replace(',', '').replace(' MB', ''));
+	var used = max - free;
+	var percentage = (used/max) * 100;
+
+	var progress_bar = $("#memory_usage_bar");
+
+	progress_bar.attr('aria-valuenow', String(used));
+	progress_bar.attr('aria-valuemax', String(max));
+
+	progress_bar.attr('style', 'width: '+percentage+'%; min-width: 2em;');
+	progress_bar.html(String(percentage) + '%');
+
+	//Check the percentage and determine what color to make the bar
+	if(percentage > 90)
+	{
+		progress_bar.removeClass('progress-bar-warning');
+		progress_bar.addClass('progress-bar-danger');
+	}
+	else if(percentage > 50)
+	{
+		progress_bar.removeClass('progress-bar-danger');
+		progress_bar.addClass('progress-bar-warning');
+	}
+	else
+	{
+		progress_bar.removeClass('progress-bar-warning');
+		progress_bar.removeClass('progress-bar-danger');
+	}
 }
