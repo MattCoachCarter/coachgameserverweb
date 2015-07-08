a392cee0a6de32dd30f57e0545a13e9a9abec743
Matt Carter <matt.coach.carter@gmail.com>
2015-07-07 22:15:34 -0500

    Fixing memory API call

--- phpapi/memory_info.php
+++ phpapi/memory_info.php
@@ -13,7 +13,7 @@
 		}
 
 		$command_output_split = explode(':', $command_output[$i]);
-		$json .= " \"".strtolower($command_output_split[0])."\": \"".trim($command_output_split[1])."\"";
+		$json .= " \"".str_replace(' ', '_', strtolower($command_output_split[0]))."\": \"".trim($command_output_split[1])."\"";
 	}
 	$json .= "}";
 
